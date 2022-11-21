a9c7e2a37812cb04b00fe8dd66f70d233d05c929
Bansi Mamtora <mbansi306@gmail.com>
2022-11-21 10:52:24 +0530

    Project setup

---
+++ DripNote/DripNote/DripNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DripNoteApp.swift
+//  DripNote
+//
+//  Created by Bansi Mamtora on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DripNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
